a8e87f84647148039d6a122056f583c8d39fdcf5
Diego Peixoto <contato@diegopeixoto.com>
2024-08-15 22:27:19 -0300

    Refactor visibility state to 'isOpen' in skeleton components
    Updated the state and handler names from 'isVisible' to 'isOpen' for better clarity and consistency. This change applies to both TopBarSkeleton and ForumSkeleton components to improve code readability.

--- src/components/Skeleton/TopBarSkeleton.tsx
+++ src/components/Skeleton/TopBarSkeleton.tsx
@@ -1,24 +1,21 @@
 import { PinnedStatus, TopBarProps } from '@/types/layout'
 import { FaChevronUp } from 'react-icons/fa6'
 
-export default function TopbarSkeleton({
-  isVisible,
-  setIsVisible,
-}: TopBarProps) {
-  function handleVisibility() {
-    setIsVisible!(!isVisible)
+export default function TopbarSkeleton({ isOpen, setIsOpen }: TopBarProps) {
+  function handleOpen() {
+    setIsOpen!(!isOpen)
   }
 
   return (
     <div
-      onClick={handleVisibility}
+      onClick={handleOpen}
       className="cursor-pointer border-t border-[#424650] bg-[#353941] flex justify-between px-[15px] items-center h-[35px] text-xs font-bold"
     >
       <span className="text-xs uppercase text-slate-200 animate-pulse px-5 w-28 rounded-md h-4 "></span>
-      <button onClick={handleVisibility}>
+      <button onClick={handleOpen}>
         <FaChevronUp
           color="#e2e8f0"
-          className={`rotate ${isVisible && 'rotate-180'}`}
+          className={`rotate ${isOpen && 'rotate-180'}`}
         />
       </button>
     </div>
